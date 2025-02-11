6a866abb7ebd85d2410da29053c71182a2e1a98f
jiyoungnim <cloundyon31@gmail.com>
2025-02-11 23:06:07 +0900

    📝 169. Creating a Server Action

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,18 +1,22 @@
-import { storePost } from '@/lib/posts';
+import { storePost } from "@/lib/posts";
 
 export default function NewPostPage() {
   async function createPost(formData) {
+    // server action
+    // 1. 비동기 함수 처리 (async) & 자동으로 객체 형태의 formData를 매개변수로 받음
+    // 2. 이 함수는 form 요소의 action prop 값으로 등록 ...리액트 프로젝트에서만 함수를 매개변수로 받을 수 있음(바닐라 JS에서 action prop = 폼제출을 처리하는 URL일 뿐!)
+    // 3. server action을 담당하는 함수는 내부에 'use server' 작성 필수
     "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
 
     storePost({
-      imageUrl: '',
+      imageUrl: "",
       title,
       content,
-      userId: 1
-    })
+      userId: 1,
+    });
   }
 
   return (

b4281784077ca2f5092ec6f56a9c0e3a98549759
jiyoungnim <cloundyon31@gmail.com>
2025-02-11 23:14:35 +0900

    📝 169. Creating a Server Action 2

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -5,7 +5,9 @@ export default function NewPostPage() {
     // server action
     // 1. 비동기 함수 처리 (async) & 자동으로 객체 형태의 formData를 매개변수로 받음
     // 2. 이 함수는 form 요소의 action prop 값으로 등록 ...리액트 프로젝트에서만 함수를 매개변수로 받을 수 있음(바닐라 JS에서 action prop = 폼제출을 처리하는 URL일 뿐!)
-    // 3. server action을 담당하는 함수는 내부에 'use server' 작성 필수
+    // 3. server action을 담당하는 함수는 내부에 'use server' 작성 필수 ... React & Next.js에게 이 함수를 등록한 form은 client form action이 아닌, server action를 수행한다고 말하는 역할의 코드
+
+    // console.log(title, image, content) // 실제로 form을 작성 후 제출버튼을 누르면, 컨솔창이 아닌 터미널에서 코드 결과가 나타나는 것을 볼 수 있음 -> 서버에서만 실행되는 서버액션이라는 것을 증명.
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");

dca5f463ca1d5de7ef4c48032ab713a5a1753d77
jiyoungnim <cloundyon31@gmail.com>
2025-02-11 23:20:49 +0900

    📝 170. Storing Data in Databases

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -7,17 +7,19 @@ export default function NewPostPage() {
     // 2. 이 함수는 form 요소의 action prop 값으로 등록 ...리액트 프로젝트에서만 함수를 매개변수로 받을 수 있음(바닐라 JS에서 action prop = 폼제출을 처리하는 URL일 뿐!)
     // 3. server action을 담당하는 함수는 내부에 'use server' 작성 필수 ... React & Next.js에게 이 함수를 등록한 form은 client form action이 아닌, server action를 수행한다고 말하는 역할의 코드
 
-    // console.log(title, image, content) // 실제로 form을 작성 후 제출버튼을 누르면, 컨솔창이 아닌 터미널에서 코드 결과가 나타나는 것을 볼 수 있음 -> 서버에서만 실행되는 서버액션이라는 것을 증명.
-    "use server";
+    // console.log(title, image, content) // ✅ 실제로 form을 작성 후 제출버튼을 누르면, 컨솔창이 아닌 터미널에서 코드 결과가 나타나는 것을 볼 수 있음 -> 서버에서만 실행되는 서버액션이라는 것을 증명.
+    ("use server");
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
 
+    // lib/posts.js 파일에 storePost 함수 정의돼있음.
     storePost({
-      imageUrl: "",
+      imageUrl: "", // 아직 이미지 URL을 가져오지 못하기 때문에 빈 스트링으로 등록
       title,
       content,
-      userId: 1,
+      userId: 1, // 로그인 메커니즘이 구현되지 못한 상태.. 일단 무조건 1으로 설정
+      // ✅ 똑같이 Form을 작성해 제출하고 나서, Feed 페이지로 가보면 방금 등록한 post가 보이는 것을 확인할 수 있다. (feed 페이지에서 getStore()를 불러와 렌더링하고 있기 때문)
     });
   }
 
